c79860c85ee1ed951c1af51a2dee38f8c9c15d32
michals <michal.m.szczepanski@gmail.com>
2024-03-18 15:05:49 +0100

    5 fixes: adapt server address and port for different profiles and access through api-gateway; split controllers to different access type

---
+++ src/main/java/work/szczepanskimichal/controller/AuthenticatedUserController.java
@@ -0,0 +1,48 @@
+package work.szczepanskimichal.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import work.szczepanskimichal.model.user.dto.UserCreateDto;
+import work.szczepanskimichal.model.user.dto.UserDto;
+import work.szczepanskimichal.model.user.dto.UserUpdateDto;
+import work.szczepanskimichal.model.user.dto.UserUpdatePasswordDto;
+import work.szczepanskimichal.service.UserService;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class AuthenticatedUserController {
+
+    private final UserService userService;
+
+    @PostMapping("")
+    public UserDto create(@RequestBody UserCreateDto userDto) {
+        return userService.createUser(userDto);
+    }
+
+    @GetMapping("/{userId}")
+    public UserDto get(@PathVariable UUID userId) {
+        return userService.getUser(userId);
+    }
+
+    @PatchMapping("/update/{userId}")
+    public UserDto update(@PathVariable UUID userId, @RequestBody UserUpdateDto dto) {
+        return userService.updateUser(userId, dto);
+    }
+
+    @PatchMapping("/update-password/{email}")
+    public ResponseEntity<String> updatePassword(@PathVariable String email,
+                                                 @RequestBody UserUpdatePasswordDto dto) {
+        userService.updatePassword(email, dto);
+        return ResponseEntity.ok("Successfully updated password");
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<String> delete(@PathVariable UUID userId) {
+        userService.deleteUser(userId);
+        return ResponseEntity.ok("Successfully deleted user");
+    }
+}
--- src/main/java/work/szczepanskimichal/exception/GlobalExceptionHandler.java
+++ src/main/java/work/szczepanskimichal/exception/GlobalExceptionHandler.java
@@ -13,49 +13,41 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(InvalidSecretKeyException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleInvalidKeyException(InvalidSecretKeyException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(SecretKeyException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleSecretKeyException(SecretKeyException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(SecretKeyNotAssignedException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleSecretKeyNotAssignedException(SecretKeyNotAssignedException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(PasswordMismatchException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handlePasswordMismatchException(PasswordMismatchException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(PasswordHashingException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handlePasswordHashingException(PasswordHashingException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidPasswordException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleInvalidPasswordException(RuntimeException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(AdminProgrammaticCreationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleAdminProgrammaticCreationException(AdminProgrammaticCreationException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidEmailException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleInvalidEmailException(InvalidEmailException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
@@ -67,31 +59,26 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(EmailDuplicationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleEmailDuplicationException(EmailDuplicationException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MissingFieldException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleMissingFieldException(MissingFieldException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(UserInactiveException.class)
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public ResponseEntity<String> handleUserInactiveException(UserInactiveException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(DuplicateKeyException.class)
-    @ResponseStatus(HttpStatus.CONFLICT)
     public ResponseEntity<String> handleDuplicateKeyException(DuplicateKeyException e) {
         return new ResponseEntity<>("Duplicate key creation encountered. " + e.getMessage(), HttpStatus.CONFLICT);
     }
